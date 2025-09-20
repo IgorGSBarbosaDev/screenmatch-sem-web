dad474373a7d0c534ba7359fe3c56c249d9f32cd
Igor Gabriel <igorgsbarbosadev@gmail.com>
2025-09-20 09:07:57 -0300

    Adiciona funcionalidade para buscar episódios a partir de uma data

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -38,6 +38,7 @@ public class Principal {
                     8 - Filtrar séries
                     9 - Buscar episódio por trecho
                     10 - Top 5 episódios por série
+                    11 - Buscar episodios a partir de uma data
                     
                     0 - Sair                                 
                     """;
@@ -76,6 +77,9 @@ public class Principal {
                     break;
                 case 10:
                     topEpisodiosPorSerie();
+                break;
+                case 11:
+                    buscarEpisodiosDepoisDeUmaData();
                 break;
                 case 0:
                     System.out.println("Saindo...");
@@ -205,6 +209,17 @@ public class Principal {
                     System.out.printf("Série: %s | Temporada: %d | Episódio: %s - %s | Avaliacao %s\n",
                             e.getSerie().getTitulo(), e.getTemporada(),
                             e.getNumeroEpisodio(), e.getTitulo(), e.getAvaliacao()));
+        }
+    }
+    private void buscarEpisodiosDepoisDeUmaData(){
+        buscarSeriePorTitulo();
+        if (serieBusca.isPresent()){
+            Serie serie = serieBusca.get();
+            System.out.println("Digite o ano limite de lancamento");
+            var anoLancamento = leitura.nextLine();
+            List<Episodio> episodiosAno = repositorio.episodiosPorSerieEAno(serie, anoLancamento);
+
+            episodiosAno.forEach(System.out::println);
         }
     }
 }
